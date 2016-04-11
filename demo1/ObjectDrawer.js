2438962fc411153b3b0412a5f9afffa01095190e
Jakub Mareda <mareda@techsys.cz>
2016-04-11 15:01:07 +0200

    added demo

---
+++ demo1/ObjectDrawer.js
@@ -0,0 +1,84 @@
+define(['pixi'], function(PIXI) {
+  function ObjectDrawer() {
+     this.width = this.height = 300;
+     this.fitScreen = this.fitScreen.bind(this);
+     this.drawLoop = this.drawLoop.bind(this);
+     this._zoom = 1;
+  }
+  
+  ObjectDrawer.prototype.constructor = ObjectDrawer;
+  ObjectDrawer.prototype.createScreen = function() {
+
+    this.fitScreen();
+    this.renderer = PIXI.autoDetectRenderer(this.width, this.height,{backgroundColor : 0x0});  
+    window.addEventListener("resize", this.fitScreen);
+    document.body.appendChild(this.renderer.view);
+    
+    // create the root of the scene graph
+    this.stage = new PIXI.Container();
+    this.stage.interactive = true;
+    //this.hud = new PIXI.Container();
+    
+    this.hud_graphics = new PIXI.Graphics();
+    this.stage.addChild(this.hud_graphics);
+    // call again to align stage properly
+    this.fitScreen();
+  }
+  ObjectDrawer.prototype.fitScreen = function() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    if(this.renderer) {
+        this.renderer.resize(this.width = window.innerWidth, this.width = window.innerHeight);
+        if(this.stage) {
+            this.stage.position.x = this.renderer.width/2;
+            this.stage.position.y = this.renderer.height/2;
+        }
+    }
+  }
+  ObjectDrawer.prototype.drawLoop = function() {
+    if(this.focusPoint) {
+      this.stage.pivot.x = this.focusPoint.x;
+      this.stage.pivot.y = this.focusPoint.y;
+    }
+    this.renderer.render(this.stage);
+    requestAnimationFrame(this.drawLoop);
+  }
+  
+  Object.defineProperty(ObjectDrawer.prototype, "zoom", {
+    get:function( ){return this._zoom;},
+    set:function(x){
+      this._zoom = x;
+      if(this.stage) {
+        this.stage.scale.x = x;
+        this.stage.scale.y = x;
+      }
+    }
+  });
+  
+  
+  ObjectDrawer.textures = {
+    asteroid: PIXI.Texture.fromImage("asteroid.png"),
+    spaceship: PIXI.Texture.fromImage("spaceship.png"),
+    projectile: PIXI.Texture.fromImage("projectile_circle.png"),
+  }
+  
+  ObjectDrawer.FocusPoint = function(x, y) {
+    if(typeof x=="object" && x.x) {
+      y = x.y;
+      x = x.x;    
+    }
+    this.x = x;
+    this.y = y;
+  }
+  ObjectDrawer.FocusPointObject = function(object) {
+    this.object = object;
+  }
+  Object.defineProperty(ObjectDrawer.FocusPointObject.prototype, "x", {
+    get:function( ){return this.object.x;},
+  });
+  Object.defineProperty(ObjectDrawer.FocusPointObject.prototype, "y", {
+    get:function( ){return this.object.y;},
+  });
+  
+  return ObjectDrawer;
+});
---
+++ demo1/ObjectManager.js
@@ -0,0 +1,47 @@
+define(["Spaceship"], function(Spaceship) {
+  function ObjectManager(remote) {
+    this.MAX_ID = 0;
+    this.array = [];
+    this.byId = {};
+    this.remote = remote;
+  }
+  /**
+   * Accepts an array of aray indexes where objects should bedeleted. Returns array of IDs of deleted objects**/
+  ObjectManager.prototype.destroyObjects = function(indexes) {
+    var ids = [];
+    var array = this.array;
+    var byId = this.byId;
+    for(var i=0,l=indexes.length; i<l; i++) {
+      var obj = array[indexes[i]];
+      array.splice(indexes[i], 1);
+      delete byId[obj.id];
+      ids.push(obj.id);
+      obj.destroy();
+    }
+    var array = new Uint32Array(ids);
+    //console.log("[WORKER->MAIN] Destroy: ",destroy);
+    this.remote.postMessage({name:"destroy", data: array.buffer}, [array.buffer]);
+    return ids;
+  }
+  /** Creates new object and pushes it into remote target **/
+  ObjectManager.prototype.addObject = function(obj) {
+    obj.id = ++this.MAX_ID;
+    obj.manager = this;
+    this.byId[obj.id] = obj;
+    this.array.push(obj);
+    var array = obj.saveToArray();
+    this.remote.postMessage({name:"create", data: array.buffer, constructors:[obj.constructor.name]}, [array.buffer]);
+  }
+  /** Assumes an object was received from remote target and adds it to local database **/
+  ObjectManager.prototype.receiveObject = function(obj) {
+    this.MAX_ID = obj.id;
+    obj.manager = this;
+    this.byId[obj.id] = obj;
+    this.array.push(obj);
+    if(obj instanceof Spaceship) {
+      console.log("Ship ID: ", obj.id);
+    }
+  }
+  
+  return ObjectManager;
+});
